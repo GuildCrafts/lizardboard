ae89f1de84a184db36ab3e11de5dff9fe8c84b8b
Amelia Clarke <ameliavoncat@gmail.com>
2016-11-07 17:53:26 -0800

    Created schema file for user widgets

---
+++ database/schema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/lizardboard')
+const db= mongoose.connection
+db.on('error', console.error.bind(console,'connection error:'))
+db.once('open', function() {
+  console.log('connected')
+})
+
+
+const widgetSchema = mongoose.Schema ({
+  type: String, title: String, size: String, contents: String
+})
+const userSchema = mongoose.Schema ({
+  username: String,
+  widgets: [ widgetSchema ]
+})
+const userWidgets = mongoose.model('userWidgets', userSchema)
+
+module.exports = {
+  userWidgets
+}
