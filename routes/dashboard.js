ac44acb2b27b98fb84a4f77eb2761ef0a1bb544a
Harman Singh <harmanlearns@gmail.com>
2016-11-08 18:07:45 -0800

    All CRUD functions work in routes/dashboard.js

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,28 +1,43 @@
-const mongoose = require('mongoose');
-const Dashboard = require('../models/dashboard.js');
+const mongoose = require('mongoose')
+const Dashboard = require('../models/dashboard.js')
 
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 
 mongoose.connect('mongodb://localhost/lizardboard')
   .then(() => console.log('connection succesful'))
-  .catch((error) => console.error(err));
+  .catch((error) => console.error(err))
 
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 router.post('/', (request, response, next) => {
   Dashboard.create(request.body, (error, post) => {
     if (error)
-      return next(error);
-    response.json(post);
-  });
-});
+      return next(error)
+    response.json(post)
+  })
+})
 
 router.get('/:id', (request, response, next) => {
   Dashboard.findById(request.params.id, (error, post) => {
     if (error) return next(error);
     response.json(post);
-  });
-});
+  })
+})
 
-module.exports = router;
+router.put('/:id', (request, response, next) => {
+  Dashboard.findByIdAndUpdate(request.params.id, request.body, (error, post) => {
+    if (error) return next(error)
+    response.json(post)
+  })
+})
+
+router.delete('/:id',(request, response, next) => {
+  console.log(request.params.id);
+  Dashboard.findByIdAndRemove(request.params.id, request.body, (error, post) => {
+    if (error) return next(error)
+    response.json(post)
+  })
+})
+
+module.exports = router
