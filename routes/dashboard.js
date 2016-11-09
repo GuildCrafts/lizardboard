ef4e013d4183c6aa5e3cc46f88eb246822aff80d
Harman Singh <harmanlearns@gmail.com>
2016-11-08 16:37:28 -0800

    Create new dashboard and read by ObjectId work

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,5 +1,3 @@
-const express = require('express');
-const router = express.Router();
 
 const mongoose = require('mongoose');
 const Dashboard = require('../models/dashboard.js');
@@ -11,16 +9,23 @@ mongoose.connect('mongodb://localhost/lizardboard')
   .then(() => console.log('connection succesful'))
   .catch((error) => console.error(err));
 
-router.get('/:id', (request, response ) => {
-  const { id } = request.params
-  console.log('id', id);
-  response.json(id);
-});
+const express = require('express');
+const router = express.Router();
 
+
+//CREATE
 router.post('/', (request, response, next) => {
   Dashboard.create(request.body, (error, post) => {
     if (error)
       return next(error);
+    response.json(post);
+  });
+});
+
+//READ
+router.get('/:id', (request, response, next) => {
+  Dashboard.findById(request.params.id, (error, post) => {
+    if (error) return next(error);
     response.json(post);
   });
 });
