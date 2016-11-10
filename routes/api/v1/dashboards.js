aa5399900e407ba9b0cbd855e4329175c289ccae
Harman Singh <harmanlearns@gmail.com>
2016-11-10 11:09:20 -0800

    WIP deleted routes/api/v1/widgets file, added creation of widgets to routes/api/v1/dashboards file

--- routes/api/v1/dashboards.js
+++ routes/api/v1/dashboards.js
@@ -1,6 +1,5 @@
 const Dashboard = require('../../../models/dashboards.js')
 const { Widget } = require('../../../models/widgets.js')
-const User = require('../../../models/users.js')
 
 const express = require('express')
 const router = express.Router()
@@ -19,37 +18,10 @@ router.post('/', (request, response, next) => {
   })
 })
 
-router.post('/:id/widgets', (request, response, next) => {
-  Dashboard.findById(request.params.id, (error, dashboard) => {
-    if (error) return next(error)
-    let newWidget = new Widget(request.body)
-    dashboard.widgets.push(newWidget)
-    dashboard.save((error) => {
-      if (error) return next(error)
-    })
-    response.json(dashboard)
-  })
-  // .then( dashboard.widgets.create(request.body, (error, dashboard) => {
-  //   if (error) return next(error)
-  //   response.json(post)
-  // }))
-    // .then
-    // BlogPost.findById(myId, function (err, post) {
-    //   if (!err) {
-    //     post.comments[0].remove();
-    //     post.save(function (err) {
-    //       // do something
-    //     });
-    //   }
-    // });
-    // dashboards.widgets.create(request.body)
-    // Widget.create(request.body,)
-})
-
 router.get('/:id', (request, response, next) => {
   Dashboard.findById(request.params.id, (error, post) => {
-    if (error) return next(error);
-    response.json(post);
+    if (error) return next(error)
+    response.json(post)
   })
 })
 
@@ -66,6 +38,21 @@ router.delete('/:id',(request, response, next) => {
   Dashboard.findByIdAndRemove(request.params.id, request.body, (error, post) => {
     if (error) return next(error)
     response.json(post)
+  })
+})
+
+router.post('/:id/widgets', (request, response, next) => {
+  Dashboard.findById(request.params.id, (error, dashboard) => {
+    if (error) return next(error)
+
+    let newWidget = new Widget(request.body)
+
+    dashboard.widgets.push(newWidget)
+
+    dashboard.save((error) => {
+      if (error) return next(error)
+    })
+    response.json(dashboard)
   })
 })
 
