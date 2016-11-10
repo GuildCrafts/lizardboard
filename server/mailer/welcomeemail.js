d45383171269aaaa56e277cce9b1db27ea4af273
Amelia Clarke <ameliavoncat@gmail.com>
2016-11-10 12:14:19 -0800

    Email and password for mailer are now found in .env

--- server/mailer/welcomeemail.js
+++ server/mailer/welcomeemail.js
@@ -1,23 +1,22 @@
 const nodemailer = require('nodemailer')
 const smtpTransport = require('nodemailer-smtp-transport')
 
-
-
-const mailer = nodemailer.createTransport(smtpTransport('smtps://lizardboardmailer@gmail.com:lizardmail!@smtp.gmail.com'))
+const mailer = nodemailer.createTransport(smtpTransport(`smtps://${process.env.MAILER_EMAIL}:${process.env.MAILER_PASSWORD}@smtp.gmail.com`))
 
 const sendWelcomeEmail = user => {
   console.log(user)
   console.log('sending email')
+  console.log(process.env.MAILER_PASSWORD)
   return mailer.sendMail( welcomeEmail(user.email, user.username) )
 }
 
 const welcomeEmail = ( userEmail, userName ) => {
   return {
-    from: '"LizardBoard" <noreply@lizardboard.com>',
+    from: '"Lizardboard" <noreply@lizardboard.com>',
     to: userEmail,
     subject: `Are You Ready For Lizards, ${userName}?`,
-    text: "Welcome to Lizardboard, we have lizards.",
-    html: "<p>Lizards~!</p>"
+    text: "Welcome to Lizardboard",
+    html: "<p>We have so many lizards.</p>"
   }
 }
 
